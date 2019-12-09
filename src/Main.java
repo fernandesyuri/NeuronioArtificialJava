ebd52510824d6927c45023ae2be60e0c24a207ce
Otavio <otaviopalma@gmail.com>
2019-12-09 15:26:20 -0300

    RV2

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,18 @@ public class Main {
         FuncaoAtivacao fLimiar = new FuncaoLimiar();
         FuncaoAtivacao fSigmoide = new FuncaoSigmoide();
 
-        // double dados[][] = {{1, 1}, {1, 4}, {4, 1}, {4, 4}};
-        // double saida[] = {0, 0, 1, 1};
-        double dados[][] = { { -1, 0 }, { -1, 1 }, { 1, 3 }, { 0, 5 }, { -1, 3 } };
-        double saida[] = { 0, 0, 0, 1, 1 };
+        double dados[][] = {{1, 1}, {1, 4}, {4, 1}, {4, 4}};
+        double saida[] = {0, 0, 1, 1};
+        // double dados[][] = { { 0, 0 }, { 0, 1 }, { 1, 0 }, { 1, 1 } };
+         //double saida[] = { 0, 0, 0, 1 };
 
         Perceptron p = new Perceptron(2);
         p.setFuncaoAtivacao(fLimiar);
-        p.setNumeroEpocas(1000);
+        p.setNumeroEpocas(100);
         p.setTaxaAprendizado(0.2);
         p.aprendizado(dados, saida);
 
-        double entrada[] = { -1, 3 };
+        double entrada[] = { 4, 4 };
         double resposta = p.getSaida(entrada);
         System.out.println("y = " + resposta);
     }
